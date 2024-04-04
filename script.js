a0b12cbc49fcefcb88b8b8e59d7de86fccd1cf59
Dhruv Mehta <Dhruv.Mehta@finova.tech>
2024-04-04 09:17:06 +0530

    Only Local Storage Remaining Rest Completed

--- script.js
+++ script.js
@@ -61,6 +61,45 @@ function clearAllItems(e) {
   checkUI();
 }
 
+// function filterItems(e) {
+//   const items = itemList.querySelectorAll("li");
+//   const text = e.target.value.toLowerCase();
+
+//   console.log(`Your Text is : ${text}`);
+//   console.log(`Your Target is : ${e.target}`);
+//   console.log(e.target);
+
+//   items.forEach((item) => {
+//     const itemName = item.textContent.toLowerCase();
+//     console.log(itemName);
+
+//     if (itemName.indexOf(text) != -1) {
+//       item.style.display = "flex";
+//     } else {
+//       item.style.display = "none";
+//     }
+//   });
+
+//   console.log(text);
+// }
+
+function filterItems(e) {
+  const text = e.target.value.toLowerCase();
+  console.log(text);
+
+  const itemName = document.querySelectorAll("li");
+
+  itemName.forEach((items) => {
+    const filteredItems = items.firstChild.textContent.toLowerCase();
+
+    if (filteredItems.indexOf(text) != -1) {
+      items.style.display = "flex";
+    } else {
+      items.style.display = "none";
+    }
+  });
+}
+
 function checkUI() {
   console.log(itemList.children.length);
   if (itemList.children.length === 0) {
@@ -72,5 +111,6 @@ function checkUI() {
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 clearAll.addEventListener("click", clearAllItems);
+filter.addEventListener("input", filterItems);
 
 checkUI();
